1311bf17e17ba8e855051ebcc10ab3e038786721
Jatin123 <talktojatin3052003@gmail.com>
2023-02-20 18:08:26 +0530

    final commit

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const url='mongodb://0.0.0.0:27017/iNotes';
+const url = 'mongodb+srv://js223675:<password>@cluster0.0erfj6d.mongodb.net/?retryWrites=true&w=majority';
 
 const connectToDb=async()=>{
 
